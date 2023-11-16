d67634a7de5975860cbeec227f8d3d07151a76e7
jrijul1201 <jrijul1201@gmail.com>
2023-11-17 00:53:18 +0530

    minor

--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.views import trigger_report, get_report
 
 urlpatterns = [
-    path("trigger_report/", trigger_report),
-    path("get_report/<int:report_id>/", get_report),
+    path("trigger-report/", trigger_report),
+    path("get-report/<int:report_id>/", get_report),
 ]
